c660a17af0f2a1b259025fdfb4dbfc5ad1d28046
shahirsidik <122286704+shahirsidik@users.noreply.github.com>
2023-03-31 02:35:26 +0800

    Transfer Javadoc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"my.uum","l":"MagicCopyFormula"},{"p":"my.uum","l":"MagicCopyGUI"},{"p":"my.uum","l":"MagicCopyMethods"},{"p":"my.uum","l":"MagicCopyTeamMembers"}];updateSearchResults();
